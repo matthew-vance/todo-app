0467158acd891f9b0dd2947a73eea83dcf3d19e1
Matthew Vance <matthew.vance@accenture.com>
2020-01-13 11:54:23 -0500

    add typescript and linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    quotes: [2, "double"],
+    semi: [2, "always"],
+    "comma-dangle": [2, "always-multiline"],
+  },
+  ignorePatterns: ["node_modules/"],
+};
