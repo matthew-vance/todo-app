3c9bc19fd409982c4333350dbaccdd23369d56fe
Matthew Vance <matthew.vance@accenture.com>
2020-01-14 09:59:01 -0500

    add react and jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,29 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
   extends: [
     "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:jest/style",
   ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react", "jest"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
     quotes: [2, "double"],
     semi: [2, "always"],
